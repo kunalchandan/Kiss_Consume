9dfc6ece98d8655acb6646b4e0a39afd0936741d
Kunal Chandan <kunal_chandan@outlook.com>
2019-07-23 16:16:38 -0400

    Support crawling of Kiss Manga and Kiss Comics

--- kiss.py
+++ kiss.py
@@ -13,6 +13,10 @@ from typing import List, Tuple
 Issues_Links = List[Tuple[str, str]]
 Links = List[str]
 
+SUPPORTED_SITES = {'comics' : 'https://readcomiconline.to/Comic/',
+                   'manga'  : 'https://kissmanga.com/Manga/'}
+#
+
 
 def define_parser() -> argparse.ArgumentParser:
     # Define CLI Parser arguments
@@ -21,15 +25,19 @@ def define_parser() -> argparse.ArgumentParser:
                     ' scraper is purely for educational purposes. Specify the title and the skip(from the top) into the'
                     ' arguments and watch it slowly download the comics into the folder down/ISSUE_TITLE .')
     req = parser.add_argument_group('Required Arguments')
-    req.add_argument('-s', '--skip',
-                     help='Skip number of comics from top, default=0',
-                     default=0,
-                     type=int,
-                     required=True)
     req.add_argument('-t', '--title',
                      help='title of webcomic, get from the actual link of the comic, '
                           'I might implement searching (probably not)',
                      required=True)
+    req.add_argument('--consume',
+                     help='The media to scrape, this is one of {}'.format(list(SUPPORTED_SITES.keys())),
+                     choices=list(SUPPORTED_SITES.keys()),
+                     required=True)
+
+    parser.add_argument('-s', '--skip',
+                        help='Skip number of comics from top, default=0',
+                        default=0,
+                        type=int)
     parser.add_argument('-H', '--human',
                         action='store_true',
                         help='Pause to let you take care of the Google Captcha, '
@@ -37,8 +45,8 @@ def define_parser() -> argparse.ArgumentParser:
     return parser
 
 
-def define_driver(title: str) -> webdriver:
-    base_url = 'https://readcomiconline.to/Comic/'
+def define_driver(site: str, title: str) -> webdriver:
+    base_url = SUPPORTED_SITES[site]
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get(base_url + title)
     return driver
@@ -54,14 +62,28 @@ def get_links(driver: webdriver) -> Issues_Links:
     return issue_links
 
 
-def get_image_links(driver: webdriver) -> Links:
-    image_links = []
-    regex = re.compile('lstImages.push\\("(.*?)"')
+def get_image_links(driver: webdriver, site: str) -> Links:
 
-    for a in driver.find_elements_by_tag_name('script'):
-        img_set = re.findall(regex, a.get_attribute('innerHTML'))
-        if not img_set == []:
-            image_links.append(img_set)
+    # On a side note, I am genuinely surprised how they managed to obfuscate the images on each of their sites.
+    # On KissComics, they have all the links in some script located in the page, this script holds the image links
+    # On KissManga, they have img tags that somehow only load the image links once the page is loaded in a browser
+    # viewing the raw HTML leads to the images somehow not being there, I don't know enough WebDev to say how they do it
+    image_links = []
+    if site == 'comics':
+        regex = re.compile('lstImages.push\\("(.*?)"')
+
+        for a in driver.find_elements_by_tag_name('script'):
+            img_set = re.findall(regex, a.get_attribute('innerHTML'))
+            if not img_set == []:
+                image_links.append(img_set)
+
+    elif site == 'manga':
+        elements = driver.find_elements_by_xpath('//img[@onerror="onErrorImg(this)"]')
+
+        for elem in elements:
+            src = elem.get_attribute('src')
+            image_links.append(src)
+
     print(image_links)
     return np.array(image_links).flatten().tolist()
 
@@ -82,8 +104,8 @@ def zip_images(path, output_comic):
             output_comic.write(os.path.join(root, file))
 
 
-def download_issues(skip: int, title: str, human):
-    driver = define_driver(title)
+def download_issues(site: str, title: str, skip: int, human):
+    driver = define_driver(site, title)
     time.sleep(6)
     issue_links = get_links(driver)
     issue_links = issue_links[skip:]
@@ -92,7 +114,7 @@ def download_issues(skip: int, title: str, human):
     for i in range(len(issue_links)):
         driver.get(issue_links[i][0] + '&quality=hq&readType=1')
         # Gathering image links
-        image_links = get_image_links(driver)
+        image_links = get_image_links(driver, site)
 
         if len(image_links) == 0:
             if not human:
@@ -101,9 +123,9 @@ def download_issues(skip: int, title: str, human):
                 return i + skip
             else:
                 input('Pausing to allow you to take care of captcha')
-                image_links = get_image_links(driver)
+                image_links = get_image_links(driver, site)
 
-        output_folder = re.sub(r'[\\/*?:"<>|]', "", issue_links[i][1])
+        output_folder = re.sub(r'[\\/*?:"<>|\ ]', "", issue_links[i][1]).replace('..', '_').replace(' ', '-')
         generate_output_folder(output_folder)
         # Saving Images
         save_images(image_links, output_folder)
@@ -118,15 +140,18 @@ def download_issues(skip: int, title: str, human):
 
 def main():
     args = define_parser().parse_args()
-    title = args.title
+    site = args.consume
     skip = args.skip
+    title = args.title
     human = args.human
     incomplete = True
     while incomplete:
-        skip = download_issues(skip, title, human)
+        skip = download_issues(site, title, skip, human)
         if skip == -1:
             incomplete = False
         else:
+            # This is some sleep value to pause and hope they forget about us
+            # (for not dealing with captcha)
             time.sleep(30)
 
 
